87e8a90067bbd942ef6f38edd1e31447e3ccced7
Make310 <luisfmarquez1999@gmail.com>
2022-03-24 09:48:33 -0500

    logo y titulo modificado 2

--- src/modules/sheldon/components/Navbar.vue
+++ src/modules/sheldon/components/Navbar.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="hero">
-    <div class="container">
-      <div
-        class="d-flex justify-content-center align-items-center flex-column flex-md-row pt-5"
-        data-aos="fade-up"
-        data-aos-duration="2000"
-      >
-        <img src="@/assets/logo.png" alt="Genius Logo" />
-        <h1 class="top__font">SHELDON</h1>
-      </div>
+    <div
+      class="d-flex justify-content-center align-items-center flex-column flex-md-row pt-5"
+      data-aos="fade-down-right"
+      data-aos-duration="2000"
+    >
+      <img src="@/assets/logo.png" alt="Genius Logo" />
+      <h1 class="top__font">SHELDON</h1>
     </div>
   </div>
 </template>
@@ -21,15 +19,18 @@ export default {};
 <style lang="scss" scoped>
 .hero {
   background-image: url("../../../assets/hero.jpg");
-  height: 35rem;
+  background-color: #00000070;
+  background-blend-mode: soft-light;
+  height: 30rem;
   background-repeat: none;
   background-size: cover;
 }
 
 .top__font {
+  font-family: "Kalam", cursive;
   font-size: 5rem;
   font-weight: bold;
-  color: #795d44;
+  color: white;
 }
 
 @media (min-width: 768px) {
@@ -39,6 +40,6 @@ export default {};
 }
 
 img {
-  height: 15rem;
+  height: 12rem;
 }
 </style>
